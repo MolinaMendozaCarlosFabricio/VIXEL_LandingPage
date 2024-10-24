7f922f1d586c41e7a7342a45d3ac9aa6f21b81f8
JosephAntony37900 <Joseantoniopintoaguilar@gmail.com>
2024-10-23 22:55:11 -0600

    Firma hecha

---
+++ src/components/Plantillas/JoinInAGlobalTournamentMain/JoinInAGlobalTournamentMain.tsx
@@ -0,0 +1,84 @@
+import { Button } from '@gear-js/vara-ui';
+import "./JoinInAGlobalTournamentMain.css";
+import ImgPrueba from "../../../img/hero-banner.jpg"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import ButtonPay from "@/components/Atoms/ButtonPay/ButtonPay";
+import { useSailsCalls } from '@/app/hooks';
+import { useAccount, useAlert } from '@gear-js/react-hooks';
+import { Codec, CodecClass, Signer } from '@polkadot/types/types';
+import { HexString } from '@gear-js/api';
+import { web3FromSource } from '@polkadot/extension-dapp';
+
+export default function JoinInAGlobalTournamentMain (){
+
+    const { account } = useAccount();
+    const sails = useSailsCalls();
+    const alert = useAlert();
+
+    const getUserSigner = (): Promise<[HexString, Signer]> => {
+        return new Promise(async (resolve, reject) => {
+            if (!account) {
+                alert.error("Accounts not ready!");
+                reject('Account not ready!');
+                return;
+            }
+
+            const { signer } = await web3FromSource(account.meta.source);
+            const temp = (signer as string | CodecClass<Codec, any[]>) as Signer;
+            resolve([account.decodedAddress, temp]);
+        })
+    }
+
+    const objTournament = {
+        title: "Torneo mundial de tetris!",
+        game: "Tetris",
+        description: "Campeones de tetris, preparen sus dedos porque se viene el torneo de tetris, donde competirán contra los demás para sobrevivir a la oleada de figuras, buena suerte campeones!",
+        srcPhoto: ImgPrueba
+    }
+
+    return(
+        <main className="boyd-join-in-a-tournament">
+            <h2 className="justify-this-content">Agregar torneo <FontAwesomeIcon icon={faTrophy}/></h2> 
+            <div className="content-info-tournament-to-join">
+                <h2>{objTournament.title}</h2>
+                <img src={objTournament.srcPhoto}/>
+                <h4>{objTournament.game}</h4>
+                <p>{objTournament.description}</p>
+
+                <Button onClick={async () => {
+                if (!sails) {
+                    alert.error('SailsCalls is not ready!');
+                    return;
+                }
+
+                const [ userAddress, signer ] = await getUserSigner();
+
+                try {
+                    const response = await sails.command(
+                        'Ping/Pong',
+                        {
+                            userAddress,
+                            signer
+                        },
+                        {
+                            callbacks: {
+                                onSuccess() { alert.success('Message send!') },
+                                onError() { alert.error('Error while sending message!') },
+                                onBlock(blockHash) { alert.info(`Message in block: ${blockHash}`) },
+                                onLoad() { alert.info('Will send a message') },
+                            }
+                        }
+                    );
+
+                    console.log(`Response: ${Object.keys(response)[0]}`);
+                } catch (e) {
+                    alert.error('Error while sending message');
+                }
+            }}>
+                Send Pong
+            </Button>
+            </div>
+        </main>
+    );
+}
